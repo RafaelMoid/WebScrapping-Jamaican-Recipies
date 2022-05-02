5867758cfb219cfea09313a315e70bead95e091f
Rafael Varela <rafaelvrcr@gmail.com>
2022-05-02 12:25:25 -0300

    Error to fix

--- app/src/main/java/com/example/webscrapperjamaicanrecipies/MainActivity.kt
+++ app/src/main/java/com/example/webscrapperjamaicanrecipies/MainActivity.kt
@@ -2,12 +2,20 @@ package com.example.webscrapperjamaicanrecipies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.doAsync
+import org.jsoup.Jsoup
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        
+        doAsync {
+
+            var doc = Jsoup.connect("https://jamaicans.com/recipes/").get()
+            var allinfo = doc.getElementsByClass("main-box-inside ")
+            System.out.println(allinfo)
+
+        }
     }
 }
